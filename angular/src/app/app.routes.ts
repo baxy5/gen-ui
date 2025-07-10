ee577c9a90c6233d9ec893214451c3556660e929
baxy5 <bakos.soda@gmail.com>
2025-07-10 10:55:20 +0200

    removed angular

--- angular/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { DndPageComponent } from '../components/dnd-page/dnd-page.component';
-
-export const routes: Routes = [
-  {
-    path: 'dnd-page',
-    component: DndPageComponent,
-  },
-];
